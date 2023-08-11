c0e6ed32e2a4fb8b2f8b6d9263e9324ead0b942b
CourtneyS88 <137658251+CourtneyS88@users.noreply.github.com>
2023-08-11 16:11:51 -0500

    modified buttons

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+function Banner () {
+    return (
+        <header className='header'>
+            Orbit Report
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </header>
+    );
+}
+
+export default Banner;
